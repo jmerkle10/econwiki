560cbcc1b315157b9205091a95a619327a6b6aeb
Seth Donaldson <sethd63@gmail.com>
2017-10-19 20:16:28 -0600

    Working i guess

--- assets/js/magister.js
+++ assets/js/magister.js
@@ -1,4 +1,4 @@
-// global. currently active menu item 
+// global. currently active menu item
 var current_item = 0;
 
 // few settings
@@ -8,81 +8,84 @@ var section_show_time = 1300;
 // jQuery stuff
 jQuery(document).ready(function($) {
 
-	// Switch section
-	$("a", '.mainmenu').click(function() 
-	{
-		if( ! $(this).hasClass('active') ) { 
-			current_item = this;
-			// close all visible divs with the class of .section
-			$('.section:visible').fadeOut( section_hide_time, function() { 
-				$('a', '.mainmenu').removeClass( 'active' );  
-				$(current_item).addClass( 'active' );
-				var new_section = $( $(current_item).attr('href') );
-				new_section.fadeIn( section_show_time );
-			} );
-		}
-		return false;
-	});		
+  // Switch section
+  $("a", '.mainmenu').click(function() {
+    if (!$(this).hasClass('active')) {
+      current_item = this;
+      // close all visible divs with the class of .section
+      $('.section:visible').fadeOut(section_hide_time, function() {
+        $('a', '.mainmenu').removeClass('active');
+        $(current_item).addClass('active');
+        var new_section = $($(current_item).attr('href'));
+        new_section.fadeIn(section_show_time);
+      });
+    }
+    return false;
+  });
 });
 
-angular.module('Wiki', ['ui.router'])
-.config([
-'$stateProvider',
-'$urlRouterProvider',
-function($stateProvider, $urlRouterProvider) {
-  $stateProvider
-    .state('forum', {
+angular.module('Wiki', ['ui.router']).config([
+  '$stateProvider',
+  '$urlRouterProvider',
+  function($stateProvider, $urlRouterProvider) {
+    $stateProvider.state('forum', {
       url: '/forum',
       templateUrl: '/forum.html',
       controller: 'MainCtrl'
-    })
-    .state('posts', {
+    }).state('posts', {
       url: '/posts/{id}',
       templateUrl: '/posts.html',
       controller: 'PostsCtrl'
     });
 
-  $urlRouterProvider.otherwise('forum');
-}]) 
-.factory('postFactory', [function(){
-  var o = {
-    posts: []
-  };
-  return o;
-}])
-.controller('MainCtrl', [
-'$scope',
-'postFactory',
-function($scope, postFactory){
-  $scope.test = 'Hello world!';
+    $urlRouterProvider.otherwise('forum');
+  }
+]).factory('postFactory', [function() {
+    var o = {
+      posts: []
+    };
+    return o;
+  }
+]).controller('MainCtrl', [
+  '$scope',
+  'postFactory',
+  function($scope, postFactory) {
+    $scope.test = 'Hello world!';
+    $scope.posts = postFactory.posts;
+    $scope.addPost = function() {
+      if ($scope.formContent === '') {
+        return;
+      }
+      $scope.posts.push({
+					title: $scope.formTitle,
+					content: $scope.formContent,
+					comments: []
+				});
+      $scope.formContent = '';
+			$scope.formTitle = '';
+    };
 
-  $scope.posts = postFactory.posts;
+		$scope.searchForum = function($event) {
+			console.log($event);
+			for (i = 0; i < $scope.posts.size; i++) {
 
-  $scope.addPost = function(){
-    if($scope.formContent === '') { return; }
-    $scope.posts.push({
-      title: $scope.formTitle,
-      content: $scope.formContent,
-      comments: []
-    });
-    $scope.formContent = '';
-  };
+			}
+		}
+  }
+]).controller('PostsCtrl', [
+  '$scope',
+  '$stateParams',
+  'postFactory',
+  function($scope, $stateParams, postFactory) {
+    $scope.post = postFactory.posts[$stateParams.id];
 
-}])
-.controller('PostsCtrl', [
-'$scope',
-'$stateParams',
-'postFactory',
-function($scope, $stateParams, postFactory){
-  $scope.post = postFactory.posts[$stateParams.id];
-  
-  $scope.addComment = function(){
-    if($scope.body === '') { return; }
-    $scope.post.comments.push({
-      body: $scope.body,
-    });
-    $scope.body = '';
-  };
+    $scope.addComment = function() {
+      if ($scope.body === '') {
+        return;
+      }
+      $scope.post.comments.push({body: $scope.body});
+      $scope.body = '';
+    };
 
-}]);
-
+  }
+]);
